affae1062f96d13e9a45b028d5c833cd463841bf
Elizabeth Somsen <esomsen@emory.edu>
2025-06-30 17:36:25 -0400

    propogate uncertainty in s, 1000 contacts

--- Epi_characteristics_experiment/generation_time_simulation.R
+++ Epi_characteristics_experiment/generation_time_simulation.R
@@ -4,12 +4,12 @@ library(MASS)
 library(fitdistrplus)
 
 ## function to add linear interpolations between measured datapoints
-interpolation <- function(row1, row2, data, interval=0.01){
-  tmp.times <- seq(data[[row1,"dpe"]], data[[row2,"dpe"]], interval)
-  tmp.preds <- seq(data[[row1,"nw_titer"]], data[[row2,"nw_titer"]], length.out=length(tmp.times))
-  tmp.df <- data.frame(dpe = tmp.times, nw_titer = tmp.preds)
-  return (tmp.df)
-  rm(list=ls(pattern="^tmp"))
+interpolation <- function(row1, row2, data, interval){
+  fxn.times <- seq(data[[row1,"dpe"]], data[[row2,"dpe"]], interval)
+  fxn.preds <- seq(data[[row1,"nw_titer"]], data[[row2,"nw_titer"]], length.out=length(fxn.times))
+  fxn.df <- data.frame(dpe = fxn.times, nw_titer = fxn.preds)
+  return (fxn.df)
+  rm(list=ls(pattern="^fxn"))
 }
 
 ## create function for F(t)
@@ -21,20 +21,19 @@ calculate_pr_contact_pos <- function(lambda_integral){
 interpolation_interval <- 0.01
 LOD <- 1
 
-## MLE for s
-MLE_H1N1 <- 0.111
-MLE_H3N2 <- 0.047
+## probability trace for H1N1 and H3N2 from MLE
+load(file="prob.trace.Rdata")
+load(file="H1N1.MLE.logL.Rdata")
+load(file="H3N2.MLE.logL.Rdata")
 
 ## define "contact" as one hour of exposure
 exposure.length <- 1/24
 
 ## number of contacts per day
-num.contacts <- 100
+num.contacts <- 1000
 
 ## number of trials
 its <- 1000
-
-plot_colors <- color("muted")(2)
 
 set.seed(25)
 
@@ -52,10 +51,10 @@ H1N1_RC_ferrets <- H1N1_ferrets %>%
   mutate(dpe = dpi - 1)
 H1N1_recipient_names <- unique(H1N1_RC_ferrets$Ferret_ID)
 
-## generate interpolated viral loads for each contact ferret
 
-H1N1_transmission_probs <- vector("list", length(H1N1_recipient_names))
-names(H1N1_transmission_probs) <- H1N1_recipient_names
+## generate interpolated viral loads for each contact ferret
+H1N1_interpolated_titers <- vector("list", length(H1N1_recipient_names))
+names(H1N1_interpolated_titers) <- H1N1_recipient_names
 
 for (ferret in H1N1_recipient_names){
   ## extract data for each donor
@@ -67,7 +66,7 @@ for (ferret in H1N1_recipient_names){
   ## loop through all data until last timepoint
   for (t in 1:(length(tmp.data$dpe)-1)){
     ## interpolation
-    tmp.df <- rbind(tmp.df, interpolation(t, t+1, tmp.data))
+    tmp.df <- rbind(tmp.df, interpolation(t, t+1, tmp.data, interpolation_interval))
   }
   ## if no ending negative test, assume animal would have tested negative at next time
   if (tmp.df[length(tmp.df$dpe),"nw_titer"] > LOD){
@@ -81,40 +80,22 @@ for (ferret in H1N1_recipient_names){
     distinct() %>%
     mutate(dpe = as.numeric(dpe))
   ## store interpolated titers in list
-  H1N1_transmission_probs[[ferret]] <- tmp.df
+  H1N1_interpolated_titers[[ferret]] <- tmp.df
   ## tidy environment
   rm(list=ls(pattern="^tmp"))
 }
 
-## calculate probability of onwards transmission
-## could collapse this step into the next loop; as-is, code caluclates prob of transmission
-## for all time steps, which is not really necessary, but very fast on my laptop
 
-for (ferret in H1N1_recipient_names){
-  tmp.data <- H1N1_transmission_probs[[ferret]]
-  tmp.lambda_vals <- c()
-  ## calculate a constant force of infection given 1 hour exposure to each viral titer
-  for (k in 1:length(tmp.data$nw_titer)){
-    if (tmp.data[[k,"nw_titer"]] <= LOD){
-      ## if titer <= LOD, we assume that the force of infection is 0
-      tmp.lambda_vals[k] <- 0 
-    } else { ## otherwise, calculate constant lambda for one-hour exposure
-      tmp.lambda_vals[k] <- AUC(x=c(0, exposure.length), y=rep(tmp.data[k,"nw_titer"]*MLE_H1N1, 2), method="trapezoid")
-    }
+## draw s values for each simulation
+## use Metropolis-Hastings algorithm to accept or reject draws
+H1N1.alt.s.vals <- c()
+while (length(H1N1.alt.s.vals) < num.contacts*its){
+  draw <- round(runif(1, min=min(prob.trace$s), max=max(prob.trace$s)), digits=3)
+  accept.ratio <- exp(prob.trace[which(near(prob.trace$s, draw)),2] - H1N1.MLE.logL)
+  if (accept.ratio > runif(1, 0, 1)){
+    H1N1.alt.s.vals <- append(H1N1.alt.s.vals, draw)
   }
-  tmp.data$constant_foi <- tmp.lambda_vals
-  ## compute the probability of transmission given this force of infection
-  tmp.data$prob_transmission <- calculate_pr_contact_pos(tmp.data$constant_foi)
-  H1N1_transmission_probs[[ferret]] <- tmp.data
-  ## tidy environment
-  rm(list=ls(pattern="^tmp"))
 }
-
-## record individual R0s estimates for all trials
-H1N1.indv.Z <- matrix(data=NA, ncol=its, nrow=length(H1N1_recipient_names))
-## record negative binomial mu and k for each trial
-H1N1.negb.fits <- matrix(data=NA, ncol=its, nrow=2)
-rownames(H1N1.negb.fits) <- c("k", "mu")
 
 ## record individual gen time estimates for all trials
 ## 10 day experiment
@@ -125,13 +106,20 @@ for (i in 1:its){
   tmp.num.offspring <- c()
   tmp.gen.time <- c()
   for (ferret in H1N1_recipient_names){
-    tmp.data <- H1N1_transmission_probs[[ferret]]
+    tmp.data <- H1N1_interpolated_titers[[ferret]]
     ## draw random times for the contacts to occur each day, and round to match time resolution we have
     tmp.contact.times <- round(x=runif(num.contacts*10, min=seq(1, 10), max=seq(2, 11)), digits=2)
-    ## this keeps duplicate times
+    ## calculate probability of transmission at these times
     tmp.pr.transmission <- c()
     for (t in tmp.contact.times){
-      tmp.pr.transmission <- append(tmp.pr.transmission, tmp.data[[which(near(tmp.data$dpe, t)),"prob_transmission"]])
+      ## calculate a constant force of infection given 1 hour exposure
+      if (tmp.data[which(near(tmp.data$dpe, t)),"nw_titer"] >= LOD){
+        tmp.pr <- calculate_pr_contact_pos(AUC(x=c(0, exposure.length), 
+                                               y=rep(tmp.data[which(near(tmp.data$dpe, t)),"nw_titer"]*H1N1.alt.s.vals[i], 2), method="trapezoid"))
+      } else { ## if titer <= LOD, we assume that the force of infection is 0
+        tmp.pr <- 0
+      }
+      tmp.pr.transmission <- append(tmp.pr.transmission, tmp.pr)
     }
     ## draw a random number between 0-1; if transmission prob is higher than this number, contact is infected
     tmp.draws <- runif(num.contacts*10)
@@ -151,20 +139,14 @@ for (i in 1:its){
   }
   ## if offspring are generated, fit a distribution and add to tracker
   if (sum(tmp.num.offspring > 0)) {
-    ## add offspring distribution to tracker
-    H1N1.indv.Z[,i] <- tmp.num.offspring
-    ## add neg B params to tracker
-    H1N1.negb.fits[,i] <- fitdist(c(tmp.num.offspring), "nbinom", method="mle")$estimate
-    ## add gen times to tracker
     H1N1.gen.times[1:length(tmp.gen.time), i] <- tmp.gen.time
-    ## if no offspring are generated, only track Z
-  } else {
-    H1N1.indv.Z[,i] <- 0
-    H1N1.negb.fits[2,i] <- 0
   }
   ## tidy environment
   rm(list=ls(pattern="^tmp"))
 }
+
+print("H1N1 done")
+print(mean(H1N1.gen.times, na.rm=T))
 
 # H3N2 analysis -----------------------------------------------------------
 
@@ -195,10 +177,10 @@ H3N2_RC_ferrets <- H3N2_RC_ferrets %>%
   filter(Ferret_ID %in% kept_names)
 H3N2_recipient_names <- unique(H3N2_RC_ferrets$Ferret_ID)
 
-## generate interpolated viral loads for each contact ferret
 
-H3N2_transmission_probs <- vector("list", length(H3N2_recipient_names))
-names(H3N2_transmission_probs) <- H3N2_recipient_names
+## generate interpolated viral loads for each contact ferret
+H3N2_interpolated_titers <- vector("list", length(H3N2_recipient_names))
+names(H3N2_interpolated_titers) <- H3N2_recipient_names
 
 for (ferret in H3N2_recipient_names){
   ## extract data for each donor
@@ -210,7 +192,7 @@ for (ferret in H3N2_recipient_names){
   ## loop through all data until last timepoint
   for (t in 1:(length(tmp.data$dpe)-1)){
     ## interpolation
-    tmp.df <- rbind(tmp.df, interpolation(t, t+1, tmp.data))
+    tmp.df <- rbind(tmp.df, interpolation(t, t+1, tmp.data, interpolation_interval))
   }
   ## if no ending negative test, assume animal would have tested negative at next time
   if (tmp.df[length(tmp.df$dpe),"nw_titer"] > LOD){
@@ -224,40 +206,22 @@ for (ferret in H3N2_recipient_names){
     distinct() %>%
     mutate(dpe = as.numeric(dpe))
   ## store interpolated titers in list
-  H3N2_transmission_probs[[ferret]] <- tmp.df
+  H3N2_interpolated_titers[[ferret]] <- tmp.df
   ## tidy environment
   rm(list=ls(pattern="^tmp"))
 }
 
-## calculate probability of onwards transmission
-## could collapse this step into the next loop; as-is, code caluclates prob of transmission
-## for all time steps, which is not really necessary, but very fast on my laptop
 
-for (ferret in H3N2_recipient_names){
-  tmp.data <- H3N2_transmission_probs[[ferret]]
-  tmp.lambda_vals <- c()
-  ## calculate a constant force of infection given 1 hour exposure to each viral titer
-  for (k in 1:length(tmp.data$nw_titer)){
-    if (tmp.data[[k,"nw_titer"]] <= LOD){
-      ## if titer <= LOD, we assume that the force of infection is 0
-      tmp.lambda_vals[k] <- 0 
-    } else { ## otherwise, calculate constant lambda for one-hour exposure
-      tmp.lambda_vals[k] <- AUC(x=c(0, exposure.length), y=rep(tmp.data[k,"nw_titer"]*MLE_H3N2, 2), method="trapezoid")
-    }
+## draw s values for each simulation
+## use Metropolis-Hastings algorithm to accept or reject draws
+H3N2.alt.s.vals <- c()
+while (length(H3N2.alt.s.vals) < num.contacts*its){
+  draw <- round(runif(1, min=min(prob.trace$s), max=max(prob.trace$s)), digits=3)
+  accept.ratio <- exp(prob.trace[which(near(prob.trace$s, draw)),2] - H3N2.MLE.logL)
+  if (accept.ratio > runif(1, 0, 1)){
+    H3N2.alt.s.vals <- append(H3N2.alt.s.vals, draw)
   }
-  tmp.data$constant_foi <- tmp.lambda_vals
-  ## compute the probability of transmission given this force of infection
-  tmp.data$prob_transmission <- calculate_pr_contact_pos(tmp.data$constant_foi)
-  H3N2_transmission_probs[[ferret]] <- tmp.data
-  ## tidy environment
-  rm(list=ls(pattern="^tmp"))
 }
-
-## record individual R0s estimates for all trials
-H3N2.indv.Z <- matrix(data=NA, ncol=its, nrow=length(H3N2_recipient_names))
-## record negative binomial mu and k for each trial
-H3N2.negb.fits <- matrix(data=NA, ncol=its, nrow=2)
-rownames(H3N2.negb.fits) <- c("k", "mu")
 
 ## record individual gen time estimates for all trials
 ## 10 day experiment
@@ -268,13 +232,20 @@ for (i in 1:its){
   tmp.num.offspring <- c()
   tmp.gen.time <- c()
   for (ferret in H3N2_recipient_names){
-    tmp.data <- H3N2_transmission_probs[[ferret]]
+    tmp.data <- H3N2_interpolated_titers[[ferret]]
     ## draw random times for the contacts to occur each day, and round to match time resolution we have
     tmp.contact.times <- round(x=runif(num.contacts*10, min=seq(1, 10), max=seq(2, 11)), digits=2)
-    ## this keeps duplicate times
+    ## calculate probability of transmission at these times
     tmp.pr.transmission <- c()
     for (t in tmp.contact.times){
-      tmp.pr.transmission <- append(tmp.pr.transmission, tmp.data[[which(near(tmp.data$dpe, t)),"prob_transmission"]])
+      ## calculate a constant force of infection given 1 hour exposure
+      if (tmp.data[which(near(tmp.data$dpe, t)),"nw_titer"] >= LOD){
+        tmp.pr <- calculate_pr_contact_pos(AUC(x=c(0, exposure.length), 
+                                               y=rep(tmp.data[which(near(tmp.data$dpe, t)),"nw_titer"]*H3N2.alt.s.vals[i], 2), method="trapezoid"))
+      } else { ## if titer <= LOD, we assume that the force of infection is 0
+        tmp.pr <- 0
+      }
+      tmp.pr.transmission <- append(tmp.pr.transmission, tmp.pr)
     }
     ## draw a random number between 0-1; if transmission prob is higher than this number, contact is infected
     tmp.draws <- runif(num.contacts*10)
@@ -294,23 +265,13 @@ for (i in 1:its){
   }
   ## if offspring are generated, fit a distribution and add to tracker
   if (sum(tmp.num.offspring > 0)) {
-    ## add offspring distribution to tracker
-    H3N2.indv.Z[,i] <- tmp.num.offspring
-    ## add neg B params to tracker
-    H3N2.negb.fits[,i] <- fitdist(c(tmp.num.offspring), "nbinom", method="mle")$estimate
-    ## add gen times to tracker
     H3N2.gen.times[1:length(tmp.gen.time), i] <- tmp.gen.time
-    ## if no offspring are generated, only track Z
-  } else {
-    H3N2.indv.Z[,i] <- 0
-    H3N2.negb.fits[2,i] <- 0
   }
   ## tidy environment
   rm(list=ls(pattern="^tmp"))
 }
 
-mean(c(H1N1.gen.times), na.rm=T)
-
-mean(c(H3N2.gen.times), na.rm=T)
+print("H3N2 done")
+print(mean(H3N2.gen.times, na.rm=T))
 
 save.image(file="high_contact_sim.RData")
